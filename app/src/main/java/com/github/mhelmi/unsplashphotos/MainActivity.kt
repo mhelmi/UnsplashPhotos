1fb374b7924fffac194503c1c8553676ae6d191f
Muhammad Helmi <mohamedhelmy137@gmail.com>
2021-10-11 12:18:24 +0300

    initial commit

---
+++ app/src/main/java/com/github/mhelmi/unsplashphotos/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.github.mhelmi.unsplashphotos
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.NavController
+import com.github.mhelmi.unsplashphotos.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private var binding: ActivityMainBinding? = null
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        setupAppBar()
+    }
+
+    private fun setupAppBar() = binding?.apply {
+        setSupportActionBar(toolbar)
+        navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+
+    override fun onDestroy() {
+        binding = null
+        super.onDestroy()
+    }
+}
